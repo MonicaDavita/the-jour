d0afebb8e857ed768abe0a3a50d2dbdfdbd3a097
monicadavita <monicadavita19@gmail.com>
2023-01-14 12:16:46 +0700

    add:widget

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -1,11 +1,13 @@
-import React, { useContext } from 'react'
 import Link from 'next/link'
+import React, { useState, useEffect } from 'react'
+import { getCategories } from '../../services'
 
-const categories = [{ name: 'Spiritual', slug: 'spiritual' },
-{ name: 'Stories', slug: 'stories' },
-{ name: 'Academic', slug: 'academic' },
-{ name: 'Web Development', slug: 'web-dev' }]
 const Header = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        getCategories()
+            .then((newCategories) => setCategories(newCategories))
+    }, [])
     return (
         <div className='container mx-auto px-10 mb-8'>
             <div className='border-b w-full inline-block border-blue-400 py-8'>
@@ -15,17 +17,17 @@ const Header = () => {
                             The Jour
                         </span>
                     </Link>
-                    
+
                 </div>
                 <div className='hidden md:float-left md:contents'>
-                        {categories.map((category) => (
-                            <Link key={category.slug} href={'/category/${category.slug}'}>
-                                <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
-                                    {category.name}
-                                </span>
-                            </Link>
-                        ))}
-                    </div> 
+                    {categories.map((category) => (
+                        <Link key={category.slug} href={'/category/${category.slug}'}>
+                            <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+                                {category.category}
+                            </span>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     )
