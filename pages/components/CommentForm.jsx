56ea298501be1d8bc02dbb1f6f2b5a1d6a93e7f8
monicadavita <monicadavita19@gmail.com>
2023-01-18 00:05:14 +0700

    update:assets

--- pages/components/CommentForm.jsx
+++ pages/components/CommentForm.jsx
@@ -115,7 +115,7 @@ const CommentForm = ({ slug }) => {
       </div>
       {error && <p className='text-xs text-red-500'>All fields are required</p>}
       <div className='mt-8'>
-        <button type='button' onClick={handleCommentSubmission} className='transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
+        <button type='button' onClick={handleCommentSubmission} className='transition duration-500 ease hover:bg-[#343434] inline-block bg-none text-lg font-medium rounded-full border border-[#343434] hover:text-white text-[#343434] px-8 py-3 cursor-pointer'>
           Post Comment
         </button>
         {showSuccessMessage && <span className='text-xl float-right font-semibold mt-3 text-green-500'>Comment is on review</span>}
