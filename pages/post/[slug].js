43a4b15720474c8a1e8b06c5244ff30597ec13a1
monicadavita <monicadavita19@gmail.com>
2023-01-18 20:25:44 +0700

    fix:bug

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import { getPosts, getPostDetails } from '../../services'
-import { PostDetail, Categories, PostWidget, Author, Comments, CommentForm, Loader } from '../components'
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentForm, Loader } from '../../components'
 
 const PostDetails = ({ post }) => {
     console.log(post);
