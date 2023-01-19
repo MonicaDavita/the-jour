1ec3375cb227091c9cc2a1a3b97d2ba34800db95
monicadavita <monicadavita19@gmail.com>
2023-01-19 22:04:48 +0700

    fix:idk

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -20,20 +20,22 @@ const PostWidget = ({ categories, slug }) => {
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
         {slug ? 'Related Post' : 'Recent Post'}
       </h3>
-      {relatedPosts.map((post) => {
-        <div key={post.title} className='flex items-center w-full mb-8'>
+      {relatedPosts.map((post, index) => {
+        <div key={index} className='flex items-center w-full mb-8'>
           <div className='w-16 flex-none'>
             <img alt={post.title}
               height="60px"
               width='60px'
+              unoptimized
               src={post.featuredImage.url}
+              className='align-middle rounded-full'
             />
           </div>
           <div className='flex-grow ml-4'>
             <p className='text-gray-500 font-xs'>
               {moment(post.createdAt).format('MMM DD, YYYY')}
             </p>
-            <Link href={`/post/${post.slug}`} key={post.title} className='text-md'>
+            <Link href={`/post/${post.slug}`} key={index} className='text-md'>
             {post.title}
             </Link>
             </div>
